26f400b778f492e10002fde924030c3f222503a2
Frederick Kobo <frederick.kobo@ebucks.com>
2021-09-15 14:38:20 +0200

    Added binding

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -7,12 +7,21 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.database.ContentObserver
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
+import androidx.databinding.DataBindingUtil
+import com.udacity.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
@@ -24,21 +33,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
-        registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
-
-        custom_button.setOnClickListener {
-            val checkId = findViewById<RadioGroup>(R.id.download_radio_group).checkedRadioButtonId
-            if(checkId != -1) {
-                download()
-            } else {
-                Toast.makeText(this, getString(R.string.please_select_file), Toast.LENGTH_SHORT).show();
-            }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.apply {
+            setSupportActionBar(toolbar)
+            registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
         }
     }
 
@@ -49,17 +51,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun download() {
-        val request =
-            DownloadManager.Request(Uri.parse(URL))
-                .setTitle(getString(R.string.app_name))
-                .setDescription(getString(R.string.app_description))
-                .setRequiresCharging(false)
-                .setAllowedOverMetered(true)
-                .setAllowedOverRoaming(true)
+        val request = DownloadManager.Request(Uri.parse(URL))
+            .setTitle(getString(R.string.app_name))
+            .setDescription(getString(R.string.app_description))
+            .setRequiresCharging(false)
+            .setAllowedOverMetered(true)
+            .setAllowedOverRoaming(true)
 
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-        downloadID =
-            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+        downloadID = downloadManager.enqueue(request)// enqueue puts the download request in the queue.
     }
 
     companion object {
@@ -67,5 +67,21 @@ class MainActivity : AppCompatActivity() {
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
         private const val CHANNEL_ID = "channelId"
     }
+
+    fun onLoadingButtonClick(view: View) {
+        when (binding.mainContent.downloadRadioGroup.checkedRadioButtonId) {
+            View.NO_ID ->
+                Toast.makeText(this, "Please select the file to download", Toast.LENGTH_SHORT)
+                    .show()
+            else -> {
+                download()
+            }
+        }
+    }
+
+    fun Context.getDownloadManager(): DownloadManager = ContextCompat.getSystemService(
+        this,
+        DownloadManager::class.java
+    ) as DownloadManager
 
 }
